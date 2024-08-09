fadcf4d4cf2e7063178120285d11e5177fbc2cf1
CYBER-RAJIB <muslimhakcerbd@gmail.com>
2024-08-09 19:09:04 +0600

    Update 100 Cyber odio🎧.js

--- scripts/commands/100 Cyber odio🎧.js
+++ scripts/commands/100 Cyber odio🎧.js
@@ -1,27 +1,69 @@
+const axios = require("axios");
 const fs = require("fs");
+const request = require("request");
+ 
+const link = [
+  "https://litter.catbox.moe/s1ilqg.mp3",
+ 
+];
+ 
 module.exports.config = {
-  name: "🎧",
-  version: "1.0.1",
-  permssion: 0,
-  credits: "Islamick Cyber Chat", 
-  prefix : true,
-  description: "noprefix audio",
-  category: "no prefix",
+  name: " 🎧",
+  version: "1.0.0",
+  hasPermssion: 0,
+  credits: "Islamick Chat",
+  description: "auto reply to 🎧",
+  commandCategory: "noprefix",
   usages: "🎧",
-  cooldowns: 5, 
+  cooldowns: 5,
+  dependencies: {
+    "request":"",
+    "fs-extra":"",
+    "axios":""
+  }
 };
-
-module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
-  var { threadID, messageID } = event;
-  if (event.body.indexOf("🎧")==0 || event.body.indexOf("🎧")==0 || event.body.indexOf("🎧")==0 || event.body.indexOf("🎧")==0) {
-    var msg = {
-        body: "🖤🎧",
-        attachment: fs.createReadStream(__dirname + `/cyber/npx.mp3`)
-      }
-      api.sendMessage( msg, threadID, messageID);
-    api.setMessageReaction("❤️‍🩹", event.messageID, (err) => {}, true)
-    }
+ 
+module.exports.handleEvent = async ({ api, event, Threads }) => {
+  const content = event.body ? event.body : '';
+    const body = content.toLowerCase();
+  if (body.startsWith("🎧")) {
+    const rahad = [
+      "🖤🎧",
+      "🖤🎧"
+    
+    ];
+    const rahad2 = rahad[Math.floor(Math.random() * rahad.length)];
+ 
+    const callback = () => api.sendMessage({
+      body: `${rahad2}`,
+      attachment: fs.createReadStream(__dirname + "/cache/Cyber.mp3")
+    }, event.threadID, () => fs.unlinkSync(__dirname + "/cache/Cyber.mp3"), event.messageID);
+    
+    const requestStream = request(encodeURI(link[Math.floor(Math.random() * link.length)]));
+    requestStream.pipe(fs.createWriteStream(__dirname + "/cache/Cyber.mp3")).on("close", () => callback());
+    return requestStream;
   }
-  module.exports.run = function({ api, event, client, __GLOBAL }) {
-
+};
+ 
+module.exports.languages = {
+  "vi": {
+    "on": "Dùng sai cách rồi lêu lêu",
+    "off": "sv ngu, đã bão dùng sai cách",
+    "successText": `🧠`,
+  },
+  "en": {
+    "on": "on",
+    "off": "off",
+    "successText": "success!",
   }
+};
+ 
+module.exports.run = async ({ api, event, Threads, getText }) => {
+  const { threadID, messageID } = event;
+  let data = (await Threads.getData(threadID)).data;
+  if (typeof data["🎧"] === "undefined" || data["🎧"]) data["🎧"] = false;
+  else data["🎧"] = true;
+  await Threads.setData(threadID, { data });
+  global.data.threadData.set(threadID, data);
+  api.sendMessage(`${(data["🎧"]) ? getText("off") : getText("on")} ${getText("successText")}`, threadID, messageID);
+};
